cc1142ae7e8253a2be0b59d5160abbd4b57dc4d3
Stoub <eve@no-cloud.fr>
2018-01-15 22:15:43 +0100

    edit Downloads path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/opt/pyload/pyload-config/setup.lock" ]
 then
-        sed -i "s#/opt/pyload/Downloads#/media/Downloads#g" /tmp/pyload-config/pyload.conf
+        sed -i "s#/opt/pyload/Downloads#/media/Media/Downloads#g" /tmp/pyload-config/pyload.conf
 
 fi
 
